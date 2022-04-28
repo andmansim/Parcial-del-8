234d16862b71dc90a00b0a1978a7f0fc49086b43
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-28 09:00:53 +0200

    creamos un nuevo DataFrame

--- clases/main2.py
+++ clases/main2.py
@@ -1,6 +1,10 @@
 # Main probisional para abrir y modificar el csv
 import pandas as pd
 
-df = pd.read_csv('Pokemon.csv', delimiter = ',', enconing = 'UTF-8')
+#Abrimos csv
+df = pd.read_csv('Pokemon.csv', delimiter = ',')
 print(df)
 
+#Nuevo DataFrame con las variables que nos interesan para este código
+df_new = pd.DataFrame({'#': df['#'], 'Name': df['Name'], 'HP': df['HP'], 'Attack': df['Attack'], 'Defense': df['Defense']})
+print (df_new)
