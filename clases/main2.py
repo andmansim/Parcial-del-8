77613f15ccc893d9469523f95ee49dabf8d3ae19
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-28 09:13:10 +0200

    añadimos una nueva columna

--- clases/main2.py
+++ clases/main2.py
@@ -8,3 +8,8 @@ print(df)
 #Nuevo DataFrame con las variables que nos interesan para este código
 df_new = pd.DataFrame({'#': df['#'], 'Name': df['Name'], 'HP': df['HP'], 'Attack': df['Attack'], 'Defense': df['Defense']})
 print (df_new)
+
+#añadir una nueva columna
+tipo_ataque = pd.Series(data = ['CABEZAZO', 'CODAZO', 'PATADA', 'PUÑETAZO'], index=df_new.columns, name='Tipo-Arma')
+df_new.append(tipo_ataque)
+print(df_new)
